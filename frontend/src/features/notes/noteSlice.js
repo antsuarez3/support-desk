2a512b4a385f13c90ce32134a62478ff4808b80d
Anthony Suarez <antsuarez305@gmail.com>
2022-04-21 14:49:58 -0500

    Hook Notes to backend

---
+++ frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,10 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import noteService from './noteService'
+
+const initialState = {
+  notes: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+}
